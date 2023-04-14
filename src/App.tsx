717075eb1d866a8999da7a20a12991d044dffb39
KLdevelop <drapdrop1@gmail.com>
2023-04-14 15:47:49 +0300

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import styles from 'app.module.scss';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export const App = () => <div className={styles.app}>App</div>;
+export const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<div className={styles.app}>App</div>} />
+    </Routes>
+  </Router>
+);
