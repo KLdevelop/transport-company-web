e33030b34b09a0d68d2f6eb250e2f28269010b19
KLdevelop <drapdrop1@gmail.com>
2023-04-15 09:06:08 +0300

    added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { App } from './App';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './models/store';
 
 const AppElement = document.getElementById('app') || document.createElement('app');
 const root = createRoot(AppElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
