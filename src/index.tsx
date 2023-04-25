e33b6899315e5550a2dd558933c2910edd5b3e94
KLdevelop <drapdrop1@gmail.com>
2023-04-25 09:40:10 +0300

    app is fullscreen

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import store from './models/store';
 
 const AppElement = document.getElementById('app') || document.createElement('app');
+AppElement.style.display = 'flex';
+AppElement.style.flexDirection = 'column';
+AppElement.style.minHeight = '100%';
 const root = createRoot(AppElement);
 
 root.render(
