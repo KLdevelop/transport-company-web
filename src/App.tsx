f45060523f70d9af8f276f5e73d747f405d544ef
KLdevelop <drapdrop1@gmail.com>
2023-07-15 12:36:10 +0300

    added mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import styles from 'app.module.scss';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { MainPage } from './pages';
+import 'app.module.scss';
 
 export const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<div className={styles.app}>App</div>} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   </Router>
 );
