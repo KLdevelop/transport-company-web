ae1210a5815bc1a98c0eff6062d1db4adfcf913f
KLdevelop <drapdrop1@gmail.com>
2023-07-15 12:35:53 +0300

    fixed styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,8 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './models/store';
 
-const AppElement = document.getElementById('app') || document.createElement('app');
-AppElement.style.display = 'flex';
-AppElement.style.flexDirection = 'column';
-AppElement.style.minHeight = '100%';
+const AppElement = document.getElementById('app') || document.createElement('div');
+AppElement.id = 'app';
 const root = createRoot(AppElement);
 
 root.render(
